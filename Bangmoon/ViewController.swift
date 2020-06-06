bf1e722d66afca60958dab967737032f33c92036
Junhyeon <oh.junhyeon95@gmail.com>
2020-06-06 21:28:33 +0900

    pod 설정

---
+++ Bangmoon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bangmoon
+//
+//  Created by Junhyeon on 2020/06/06.
+//  Copyright © 2020 Bangmoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Bangmoon' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Alamofire', '~> 4.8.2'
+  pod 'Then'
+  pod 'SnapKit', '~> 5.0.0'
+  pod 'Kingfisher'
+
+  # Pods for Bangmoon
+
+end
