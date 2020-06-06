42a17e72b8f912115904680beea09c660fd1b3cc
minhee <xwoud@naver.com>
2020-06-07 00:49:29 +0900

    진행중 목록 오토레이아웃 작성중

---
+++ Bangmoon/Source/ViewController/MyPage/WorkingCell.swift
@@ -0,0 +1,24 @@
+//
+//  WorkingCell.swift
+//  Bangmoon
+//
+//  Created by 김민희 on 07/06/2020.
+//  Copyright © 2020 Bangmoon. All rights reserved.
+//
+
+import UIKit
+
+class WorkingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
